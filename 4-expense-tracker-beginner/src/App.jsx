cea614fca75f66ba5af389b9dcefd5b03dbd1cc1
Reza Dianat <dianat.reza66@gmail.com>
2026-07-19 16:35:38 +0200

    collect expense data and store it in an array

--- 4-expense-tracker-beginner/src/App.jsx
+++ 4-expense-tracker-beginner/src/App.jsx
@@ -1,14 +1,19 @@
+import { useState } from "react";
 import "./App.css";
 import ExpenseForm from "./components/ExpenseForm/ExpenseForm";
 import Heading from "./components/Heading/Heading";
 
 function App() {
+  const [expenseList, setExpenseList] = useState([]);
   return (
     <>
       <Heading />
       <main>
         <div className="formWrapper">
-          <ExpenseForm />
+          <ExpenseForm
+            expenseList={expenseList}
+            setExpenseList={setExpenseList}
+          />
         </div>
       </main>
     </>
--- 4-expense-tracker-beginner/src/components/ExpenseForm/ExpenseForm.jsx
+++ 4-expense-tracker-beginner/src/components/ExpenseForm/ExpenseForm.jsx
@@ -1,7 +1,35 @@
+import { useState } from "react";
 import styles from "./ExpenseForm.module.css";
-const ExpenseForm = () => {
+const ExpenseForm = ({ setExpenseList }) => {
+  const [expenseData, setExpenseData] = useState({
+    title: "",
+    amount: "",
+    date: "",
+    category: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setExpenseData((prev) => ({ ...prev, [name]: value }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newExpense = {
+      ...expenseData,
+      id: crypto.randomUUID(),
+    };
+    setExpenseList((prev) => [...prev, newExpense]);
+    setExpenseData({
+      title: "",
+      amount: "",
+      date: "",
+      category: "",
+    });
+  };
+
   return (
-    <form className={styles.form}>
+    <form className={styles.form} onSubmit={handleSubmit}>
       <div className={styles.formGrid}>
         <div className={styles.inputGroup}>
           <label htmlFor="title">Expense title</label>
@@ -11,6 +39,8 @@ const ExpenseForm = () => {
             id="title"
             className={styles.formInput}
             placeholder="e.g. bus ticket"
+            onChange={handleChange}
+            value={expenseData.title}
           />
         </div>
         <div className={styles.inputGroup}>
@@ -21,6 +51,8 @@ const ExpenseForm = () => {
             id="amount"
             className={styles.formInput}
             placeholder="e.g. 45 "
+            onChange={handleChange}
+            value={expenseData.amount}
           />
         </div>
         <div className={styles.inputGroup}>
@@ -30,11 +62,19 @@ const ExpenseForm = () => {
             name="date"
             id="date"
             className={styles.formInput}
+            onChange={handleChange}
+            value={expenseData.date}
           />
         </div>
         <div className={styles.inputGroup}>
           <label htmlFor="category">Expense category</label>
-          <select name="category" id="category" className={styles.formSelect}>
+          <select
+            name="category"
+            id="category"
+            className={styles.formSelect}
+            onChange={handleChange}
+            value={expenseData.category}
+          >
             <option value="">Select expense category</option>
             <option value="groceries">Groceries</option>
             <option value="transportation">Transportation</option>
