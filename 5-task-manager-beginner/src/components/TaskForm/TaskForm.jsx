a453f2c24a4d1aa85513a2afb22622f6f0e1678d
Reza Dianat <dianat.reza66@gmail.com>
2026-07-25 13:49:52 +0200

    implement form validation and add styles

--- 5-task-manager-beginner/src/components/TaskForm/TaskForm.jsx
+++ 5-task-manager-beginner/src/components/TaskForm/TaskForm.jsx
@@ -1,23 +1,44 @@
 import { useState } from "react";
 import styles from "./TaskForm.module.css";
 const TaskForm = () => {
-  // States
+  // taskData state initialized with empty string to collect form data
   const [taskData, setTaskData] = useState({
     title: "",
     date: "",
     status: "",
   });
+  // taskErros state initialized with empty string to collect form errors
+  const [taskErrors, setTaskErrors] = useState({});
 
-  // Handlers
+  // Chane handler to collect form data
   const handleChange = (e) => {
     const { name, value } = e.target;
     setTaskData((prev) => ({
       ...prev,
       [name]: value,
     }));
+    setTaskErrors((prev) => ({
+      ...prev,
+      [`${name}Error`]: "",
+    }));
+  };
+  // Validating the form
+  const handleValidation = (e) => {
+    const errors = {};
+    if (!taskData.title) errors.titleError = "Task title is required";
+    if (!taskData.date) errors.dateError = "Task date is required";
+    if (!taskData.status) errors.statusError = "Select task status";
+    setTaskErrors(errors);
+    return Object.keys(errors).length === 0;
+  };
+
+  // Handling form submission
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!handleValidation()) return;
   };
   return (
-    <form className={styles.form}>
+    <form className={styles.form} onSubmit={handleSubmit}>
       <div className={styles.inputGroup}>
         <label htmlFor="title" className={styles.label}>
           Task title
@@ -26,12 +47,15 @@ const TaskForm = () => {
           type="text"
           name="title"
           id="title"
-          placeholder="e.g. pay electricity bill"
+          placeholder="e.g. pay electricity bill (max 100 characters)"
           maxLength={100}
           className={styles.input}
           onChange={handleChange}
           value={taskData.title}
         />
+        {taskErrors.titleError && (
+          <p className={styles.formError}>{taskErrors.titleError}</p>
+        )}
       </div>
       <div className={styles.inputGroup}>
         <label htmlFor="date" className={styles.label}>
@@ -46,6 +70,9 @@ const TaskForm = () => {
           onChange={handleChange}
           value={taskData.date}
         />
+        {taskErrors.dateError && (
+          <p className={styles.formError}>{taskErrors.dateError}</p>
+        )}
       </div>
       <div className={styles.inputGroup}>
         <label htmlFor="status" className={styles.label}>
@@ -63,6 +90,9 @@ const TaskForm = () => {
           <option value="pending">Pending</option>
           <option value="completed">Completed</option>
         </select>
+        {taskErrors.statusError && (
+          <p className={styles.formError}>{taskErrors.statusError}</p>
+        )}
       </div>
       <button type="submit" className={styles.submitButton}>
         Add task
