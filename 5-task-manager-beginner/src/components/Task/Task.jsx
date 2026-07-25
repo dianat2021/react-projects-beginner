03592d4d53ef32458404934e4da88e0cf12d257e
Reza Dianat <dianat.reza66@gmail.com>
2026-07-25 17:55:38 +0200

    implement toggle status feature

--- 5-task-manager-beginner/src/components/Task/Task.jsx
+++ 5-task-manager-beginner/src/components/Task/Task.jsx
@@ -7,8 +7,24 @@ const Task = ({ task, setTaskList }) => {
       }),
     );
   };
+
+  const toggleStatus = () => {
+    setTaskList((prev) =>
+      prev.map((item) => {
+        if (item.id === task.id) {
+          return {
+            ...item,
+            status: item.status === "completed" ? "pending" : "completed",
+          };
+        }
+        return item;
+      }),
+    );
+  };
   return (
-    <li className={styles.taskItem}>
+    <li
+      className={`${styles.taskItem} ${task.status === "completed" ? styles.completed : ""}`}
+    >
       <span className={styles.title}>{task.title}</span>
       <span className={styles.date}>{task.date}</span>
       <span className={styles.status}>{task.status}</span>
@@ -23,7 +39,11 @@ const Task = ({ task, setTaskList }) => {
         <button className={styles.editButton} type="button">
           Edit
         </button>
-        <button className={styles.toggleButton} type="button">
+        <button
+          className={styles.toggleButton}
+          type="button"
+          onClick={toggleStatus}
+        >
           Cross
         </button>
       </span>
