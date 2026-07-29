320097468f503ec1e2f8c756821b851f6be32005
Reza Dianat <dianat.reza66@gmail.com>
2026-07-29 13:11:36 +0200

    implement date validation

--- 6-age-calculator/src/components/AgeCalculator/AgeCalculator.jsx
+++ 6-age-calculator/src/components/AgeCalculator/AgeCalculator.jsx
@@ -1,5 +1,26 @@
+import { useState } from "react";
 import styles from "./AgeCalculator.module.css";
 const AgeCalculator = () => {
+  // States
+  const [birthDate, setBirthDate] = useState("");
+  const [birthDateError, setBirthDateError] = useState("");
+  // Handlers
+  const handleChange = (e) => {
+    setBirthDate(e.target.value);
+    setBirthDateError("");
+  };
+  const handleValidation = () => {
+    const today = Date.now();
+    const selectedDate = new Date(birthDate);
+    let error = "";
+    if (!birthDate) {
+      error = "Birthdate is required";
+    } else if (selectedDate.getTime() > today) {
+      error = "Selected date cannot be in the future";
+    }
+    setBirthDateError(error);
+    return error === "";
+  };
   return (
     <>
       <form className={styles.form}>
@@ -12,11 +33,16 @@ const AgeCalculator = () => {
             name="birthDate"
             id="birthDate"
             className={styles.input}
+            onChange={handleChange}
+            value={birthDate}
           />
         </div>
         <button type="submit" className={styles.submitButton}>
           Calculate age
         </button>
+        {birthDateError && (
+          <p className={styles.errorParagraph}>{birthDateError}</p>
+        )}
       </form>
       <div className={styles.display}>
         <p>
