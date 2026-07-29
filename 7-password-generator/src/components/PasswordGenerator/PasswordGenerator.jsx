d0283055f4b11cd42d20d2a65f7fb439d99a9ead
Reza Dianat <dianat.reza66@gmail.com>
2026-07-29 15:38:16 +0200

    generate random password and display it

--- 7-password-generator/src/components/PasswordGenerator/PasswordGenerator.jsx
+++ 7-password-generator/src/components/PasswordGenerator/PasswordGenerator.jsx
@@ -1,9 +1,32 @@
+import { useState } from "react";
 import styles from "./PasswordGenerator.module.css";
+const chars =
+  "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+-=";
 const PasswordGenerator = () => {
+  const [password, setPassword] = useState("");
+  const generateRandomNum = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  };
+  const handleGeneratePass = (e) => {
+    e.preventDefault();
+    const randomPassLength = generateRandomNum(10, 12);
+    let generatedPass = "";
+    for (let i = 0; i < randomPassLength; i++) {
+      const randomIndex = generateRandomNum(0, chars.length - 1);
+      generatedPass += chars[randomIndex];
+    }
+    setPassword(generatedPass);
+  };
   return (
     <div className={styles.wrapper}>
-      <p className={styles.password}>Click generate to start</p>
-      <button type="button" className={styles.generateButton}>
+      <p className={styles.password}>
+        {password ? password : "Click generate to start"}{" "}
+      </p>
+      <button
+        type="button"
+        className={styles.generateButton}
+        onClick={handleGeneratePass}
+      >
         Generate
       </button>
     </div>
