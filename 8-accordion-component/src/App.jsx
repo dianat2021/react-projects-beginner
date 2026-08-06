ef32f41f40194ba320f515bd69e47e5cbef2ce7d
Reza Dianat <dianat.reza66@gmail.com>
2026-08-06 16:32:49 +0200

    add icon and state to control the active and inactive

--- 8-accordion-component/src/App.jsx
+++ 8-accordion-component/src/App.jsx
@@ -3,9 +3,9 @@ import Accordion from "./components/Accordion/Accordion";
 
 function App() {
   return (
-    <>
+    <div>
       <Accordion />
-    </>
+    </div>
   );
 }
 
--- 8-accordion-component/src/components/Accordion/Accordion.jsx
+++ 8-accordion-component/src/components/Accordion/Accordion.jsx
@@ -1,22 +1,37 @@
+import { useState } from "react";
 import styles from "./Accordion.module.css";
 const Accordion = () => {
+  const [isActive, setIsActive] = useState(false);
   return (
     <div className={styles.accordionWrapper}>
       <div className={styles.accordionHeader}>
-        <button className={styles.accordionToggle}>
-          <span className={styles.accordionHeading}>Text</span>
-          <span className={styles.accordionToggleLogo}>Logo</span>
+        <button
+          className={styles.accordionToggle}
+          onClick={() => setIsActive((prev) => !prev)}
+        >
+          <span className={styles.accordionHeading}>What Is an Accordion?</span>
+          <span className={styles.accordionToggleLogo}>
+            <img
+              src="/arrow-down.svg"
+              alt="Accordion toggle arrow"
+              className={`${styles.arrowIcon} ${isActive ? styles.activeIcon : ""}`}
+            />
+          </span>
         </button>
       </div>
-      <div className={styles.accordionContent}>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt cum
-          tempore, quod porro praesentium, odio sequi vero atque libero quam
-          deleniti doloremque voluptate ad, rerum voluptas velit aut? Nostrum
-          iure sapiente, provident sint omnis magnam quia. Mollitia, totam
-          labore reprehenderit temporibus fugit nulla velit in iste quas
-          impedit. Laboriosam, autem.
-        </p>
+      <div
+        className={`${styles.accordionContent} ${isActive ? styles.accordionContentOpen : ""}`}
+      >
+        <div className={styles.accordionContentInner}>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt cum
+            tempore, quod porro praesentium, odio sequi vero atque libero quam
+            deleniti doloremque voluptate ad, rerum voluptas velit aut? Nostrum
+            iure sapiente, provident sint omnis magnam quia. Mollitia, totam
+            labore reprehenderit temporibus fugit nulla velit in iste quas
+            impedit. Laboriosam, autem.
+          </p>
+        </div>
       </div>
     </div>
   );
