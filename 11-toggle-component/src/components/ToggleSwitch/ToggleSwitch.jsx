c51f609bef9210f034cc12bbfcfc64c5aeb3e776
Reza Dianat <dianat.reza66@gmail.com>
2026-08-08 15:49:02 +0200

    implement toggle logic

--- 11-toggle-component/src/components/ToggleSwitch/ToggleSwitch.jsx
+++ 11-toggle-component/src/components/ToggleSwitch/ToggleSwitch.jsx
@@ -1,8 +1,13 @@
+import { useState } from "react";
 import styles from "./ToggleSwitch.module.css";
 const ToggleSwitch = () => {
+  const [isActive, setIsActive] = useState(false);
   return (
     <div className={styles.wrapper}>
-      <button className={styles.toggleButton}>
+      <button
+        className={`${styles.toggleButton} ${isActive ? styles.active : ""}`}
+        onClick={() => setIsActive((prev) => !prev)}
+      >
         <span className={styles.toggleCircle}></span>
       </button>
     </div>
